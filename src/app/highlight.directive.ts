413ad239d7d72820a1274d463fe99bf5cf7eb2db
ian1017 <mdawidamengo@gmail.com>
2020-05-19 01:53:37 +0300

    create custom directive that highlights text

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHightlight]'
 })
-export class HighlightDirective {
+export class HightlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { }
+
+  @HostBinding('style.backgroundColor')backgroundColor = 'transparent';
+
+  @HostListener('mouseover') mouseover(eventData: Event) {
+    this.elem.nativeElement.style.backgroundColor = 'Red';
+    this.elem.nativeElement.style.color = 'White';
+}
+
+@HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.elem.nativeElement.style.backgroundColor = 'transparent';
+    this.elem.nativeElement.style.color = 'Black';
+}
+
+  gnOninit() {
+    this.elem.nativeElement.style.backgroundColor = 'red';
+    this.elem.nativeElement.style.color = 'white';
+  }
 
 }
