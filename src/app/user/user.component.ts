5133b3c2e195ec6bf7564878e46e923ff2af8f82
ian1017 <mdawidamengo@gmail.com>
2020-05-19 01:31:01 +0300

    add method that returns repo and user details from searched name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
+import { UserService } from './../user.service';
+import { User } from './../user';
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+user: User;
+repo: Repo;
 
-  ngOnInit(): void {
+constructor(public myService: UserService, private repoService: UserService) {}
+
+searchs(searchName) {
+  this.myService.searchUser(searchName).then(
+    (success) => {
+      this.user = this.myService.getUser;
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+  this.repoService.getRepos(searchName).then(
+      (results) => {
+        this.repo = this.repoService.getRepo;
+        console.log(this.repo);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+}
+ngOnInit() {
+this.searchs('Albert-Byrone');
   }
 
 }
