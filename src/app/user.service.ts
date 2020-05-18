ea8bcd28a7b77323f1a147e68d573acca3bce57d
ian1017 <mdawidamengo@gmail.com>
2020-05-19 01:02:21 +0300

    add method that returns a new user from search name

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,5 +9,33 @@ export class UserService {
     this.getUser = new User('', '', '', '', 0, 0, 0, '', new Date());
     this.getRepo =  new Repo('', '', '', new Date(),'');
 
+  
+    searchUser(searchName: string) {
+
+      interface Response {
+        url: string;
+        login: string;
+        html_url: string;
+        location: string;
+        public_repos: number;
+        followers: number;
+        following: number;
+        avatar_url: string;
+        created_at: Date;
+      }
+  
+  
+      // get the username
+      return new Promise((resolve, reject) => {
+        this.http.get<Response>('https://api.github.com/users/' + searchName + '?access_token=' + environment.apiKey).toPromise()
+        .then((result) => {
+          this.getUser = result;
+          resolve();
+        }, (error) => {
+        console.log(error);
+        reject();
+        });
+      });
+    }
   }
 }
