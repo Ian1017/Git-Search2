fa328dd888228c52a0e3473aae18a1accffcf4e0
ian1017 <mdawidamengo@gmail.com>
2020-05-19 01:38:16 +0300

    add routes to the routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
+import { UserComponent } from './user/user.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RepoComponent } from './repo/repo.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user-prof', component: UserComponent},
+  {path: 'search-repo', component: RepoComponent},
+  {path: '', redirectTo: '/user-prof', pathMatch: 'full'},
+  {path: '**', component: NotfoundComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
