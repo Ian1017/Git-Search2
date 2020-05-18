afa5a77a30b54562d1c97c66b85629ec051c7cd6
ian1017 <mdawidamengo@gmail.com>
2020-05-19 00:31:12 +0300

    add dependancies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
+import { DatePipe } from './date.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    RepoComponent,
+    UserComponent,
+    DatePipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
