a20eefa3a4c617444bccf7a9058de196fd20b291
ian1017 <mdawidamengo@gmail.com>
2020-05-19 01:05:26 +0300

    add method that returns a new user repo from search name

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -36,6 +36,32 @@ export class UserService {
         reject();
         });
       });
+    });
+  
+    getRepos(searchName) {
+
+      interface Repos {
+        name: string;
+        html_url: string;
+        description: string;
+        forks: number;
+        atcherCounted: number;
+        language: string;
+        created_at: Date;
+      }
+      return new Promise((resolve, reject) => {
+        this.http.get<Repo>('https://api.github.com/users/' + searchName + '/repos?order=created&sort=asc?access_token=' + environment.apiKey)
+        .toPromise().then(
+          (results) => {
+            this.getRepo = results;
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+            reject();
+          }
+        );
+      });
     }
   }
-}
+
